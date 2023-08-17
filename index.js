2f49a572396801fd205d96b34c4859baef348f58
vivekmithra <vivekmithra2020@gmail.com>
2023-08-17 19:49:31 +0530

    changes

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+// Serve static files from the "public" directory
+app.use(express.static(__dirname + '/public'));
+
+// Handle incoming connections
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  // Listen for chat messages
+  socket.on('chat message', (message) => {
+    io.emit('chat message', message); // Broadcast the message to all connected clients
+  });
+
+  // Handle disconnections
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+  });
+});
+
+const PORT = process.env.PORT || 4000;
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
